b40a4da6fae0b5b9fead8c98f0f6d979d8b8ae84
arispole <spoletini.1755679@studenti.uniroma1.it>
2020-12-07 11:36:07 +0100

    analog and digital input

---
+++ adc.h
@@ -0,0 +1,6 @@
+#include <stdint.h>
+
+void ADC_init(void);
+
+uint8_t ADC_read(uint8_t pin_num); 
+
---
+++ digital_input.h
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <avr/io.h>
+
+void digInput_Init(void);
+
+uint8_t read_digInput(uint8_t pin);
+
+
+
